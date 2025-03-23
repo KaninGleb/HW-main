5ae9c883934e11cc08251a40ca79e94515edac06
Kanin Gleb <gleb.kanin@gmail.com>
2025-03-23 22:05:02 +0300

    HW4 - SuperButton fixed and improved

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -1,4 +1,4 @@
-import React, {ButtonHTMLAttributes, DetailedHTMLProps} from 'react'
+import React, {ButtonHTMLAttributes, DetailedHTMLProps, useState} from 'react'
 import s from './SuperButton.module.css'
 
 // тип пропсов обычной кнопки, children в котором храниться название кнопки там уже описан
@@ -14,23 +14,47 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
         xType,
         className,
         disabled,
+        children,
         ...restProps // все остальные пропсы попадут в объект restProps, там же будет children
     }
 ) => {
-    const finalClassName = s.button
-        // + (disabled
-        //         ? ...
-        //         : xType === 'red'
-        //             ? ...
-        + (className ? ' ' + className : '') // задачка на смешивание классов
+    const [isHovered, setIsHovered] = useState(false);
+    const [isPressed, setIsPressed] = useState(false);
+
+    // const finalClassName =
+    //     s.button +
+    //     (disabled ? s.disabled
+    //         : xType === 'red'
+    //             ? s.red + (xType === 'secondary' ? ' ' + s.secondary : '') : '') // задачка на смешивание классов
+
+    // const finalClassName =
+    //     s.button +
+    //     (xType === 'default' ? ` ${s.default}` : '') +
+    //     (disabled ? ` ${s.disabled}` : '') +
+    //     (xType === 'red' ? ` ${s.red}` : '') +
+    //     (xType === 'secondary' ? ` ${s.secondary}` : '');
+
+    const finalClassName = `${s.button} ${disabled ? s.disabled : ''} ${xType === 'red' ? s.red : xType === 'secondary' ? s.secondary : s.default}`;
+
+    const buttonText =
+        disabled ? children :
+        isPressed ? 'On Press' :
+        isHovered ? 'On Hover' : children;
 
     return (
         <button
             disabled={disabled}
             className={finalClassName}
+            onMouseEnter={() => !disabled && setIsHovered(true)}
+            onMouseLeave={() => !disabled && setIsHovered(false)}
+            onMouseDown={() => !disabled && setIsPressed(true)}
+            onMouseUp={() => !disabled && setIsPressed(false)}
+
             {...restProps} // отдаём кнопке остальные пропсы если они есть (children там внутри)
-        />
+        >
+            {buttonText}
+        </button>
     )
 }
 
-export default SuperButton
+export default SuperButton;
