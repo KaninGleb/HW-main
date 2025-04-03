5b4023b2c26500d1f1ed9120ec8223e4d8395647
Kanin Gleb <gleb.kanin@gmail.com>
2025-04-03 17:55:20 +0300

    HW5 - Layout refactored

--- src/s2-homeworks/hw05/layout/Layout.tsx
+++ src/s2-homeworks/hw05/layout/Layout.tsx
@@ -2,6 +2,7 @@ import React, { FC, ReactNode, useEffect, useState } from 'react'
 import { Header } from '../header/Header'
 import { Sidebar } from '../sidebar/Sidebar'
 
+
 type PropsType = {
     children: ReactNode
 }
@@ -13,12 +14,13 @@ export const Layout: FC<PropsType> = ({ children }) => {
 
     useEffect(() => {
         open && (document.body.style.overflow = 'hidden')
-        !open && (document.body.style.overflow = 'unset')
+        !open && (document.body.style.overflow = 'auto')
+
     }, [open]) // отключает прокрутку при открытом меню
 
     return (
         <>
-            <Sidebar open={open} handleClose={handleClose} />
+            <Sidebar open={open} handleOpen={handleOpen} handleClose={handleClose} />
             <Header handleOpen={handleOpen} />
             <div>
                 {/*страницы*/}
