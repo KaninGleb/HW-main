1eb37014fad9a2c498945761e79169a6ff703e8d
Kanin Gleb <gleb.kanin@gmail.com>
2025-04-07 22:33:50 +0300

    HW6 - SuperButton fixed

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -3,58 +3,60 @@ import s from './SuperButton.module.css'
 
 // тип пропсов обычной кнопки, children в котором храниться название кнопки там уже описан
 type DefaultButtonPropsType = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>,
-    HTMLButtonElement>
+  HTMLButtonElement>
 
 type SuperButtonPropsType = DefaultButtonPropsType & {
-    xType?: string
+  xType?: string
 }
 
 const SuperButton: React.FC<SuperButtonPropsType> = (
-    {
-        xType,
-        className,
-        disabled,
-        children,
-        ...restProps // все остальные пропсы попадут в объект restProps, там же будет children
-    }
+  {
+    xType,
+    className,
+    disabled,
+    children,
+    ...restProps // все остальные пропсы попадут в объект restProps, там же будет children
+  }
 ) => {
-    const [isHovered, setIsHovered] = useState(false);
-    const [isPressed, setIsPressed] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+  const [isPressed, setIsPressed] = useState(false);
 
-    // const finalClassName =
-    //     s.button +
-    //     (disabled ? s.disabled
-    //         : xType === 'red'
-    //             ? s.red + (xType === 'secondary' ? ' ' + s.secondary : '') : '') // задачка на смешивание классов
+  // const finalClassName =
+  //     s.button +
+  //     (disabled ? s.disabled
+  //         : xType === 'red'
+  //             ? s.red + (xType === 'secondary' ? ' ' + s.secondary : '') : '') // задачка на смешивание классов
 
-    // const finalClassName =
-    //     s.button +
-    //     (xType === 'default' ? ` ${s.default}` : '') +
-    //     (disabled ? ` ${s.disabled}` : '') +
-    //     (xType === 'red' ? ` ${s.red}` : '') +
-    //     (xType === 'secondary' ? ` ${s.secondary}` : '');
+  // const finalClassName =
+  //     s.button +
+  //     (xType === 'default' ? ` ${s.default}` : '') +
+  //     (disabled ? ` ${s.disabled}` : '') +
+  //     (xType === 'red' ? ` ${s.red}` : '') +
+  //     (xType === 'secondary' ? ` ${s.secondary}` : '');
 
-    const finalClassName = `${s.button} ${disabled ? s.disabled : ''} ${xType === 'red' ? s.red : xType === 'secondary' ? s.secondary : s.default}`;
+  const finalClassName = `${s.button} 
+    ${disabled ? s.disabled : ''} 
+    ${xType === 'red' ? s.red : xType === 'secondary' ? s.secondary : s.default}`;
 
-    const buttonText =
-        disabled ? children :
-        isPressed ? 'On Press' :
+  const buttonText =
+    disabled ? children :
+      isPressed ? 'On Press' :
         isHovered ? 'On Hover' : children;
 
-    return (
-        <button
-            disabled={disabled}
-            className={finalClassName}
-            onMouseEnter={() => !disabled && setIsHovered(true)}
-            onMouseLeave={() => !disabled && setIsHovered(false)}
-            onMouseDown={() => !disabled && setIsPressed(true)}
-            onMouseUp={() => !disabled && setIsPressed(false)}
+  return (
+    <button
+      disabled={disabled}
+      className={finalClassName}
+      onMouseEnter={() => !disabled && setIsHovered(true)}
+      onMouseLeave={() => !disabled && setIsHovered(false)}
+      onMouseDown={() => !disabled && setIsPressed(true)}
+      onMouseUp={() => !disabled && setIsPressed(false)}
 
-            {...restProps} // отдаём кнопке остальные пропсы если они есть (children там внутри)
-        >
-            {buttonText}
-        </button>
-    )
+      {...restProps} // отдаём кнопке остальные пропсы если они есть (children там внутри)
+    >
+      {buttonText}
+    </button>
+  )
 }
 
 export default SuperButton;
