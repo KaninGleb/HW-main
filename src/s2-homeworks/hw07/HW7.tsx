1dd286eb7c6322535a5692d511e5db14e0095417
Kanin Gleb <gleb.kanin@gmail.com>
2025-04-16 12:34:43 +0300

    HW7 - HW7 Typification simplified

--- src/s2-homeworks/hw07/HW7.tsx
+++ src/s2-homeworks/hw07/HW7.tsx
@@ -9,11 +9,10 @@ import s from './HW7.module.css'
 // * 2 - в файле SuperRadio.tsx дописать логику функции onChangeCallback
 // * 3 - в файле SuperRadio.tsx дописать name, checked, value (узнать для чего в радио name)
 // * 4 - сделать стили в соответствии с дизайном
-export type ValueType = 'Pre-junior' | 'Junior'| 'Junior +';
 
 export type OptionType = {
-  id: number,
-  value: 'Pre-junior' | 'Junior'| 'Junior +'
+  id: number
+  value: any
 }
 
 const arr: OptionType[] = [
