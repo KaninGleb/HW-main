6bd4b5b8d04665df6af5b0f34152891ebfeccd58
Kanin Gleb <gleb.kanin@gmail.com>
2025-04-24 18:36:10 +0300

    HW8 - homeWorkReducer

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,31 @@
 import {UserType} from '../HW8'
 
+
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+  | { type: 'sort'; payload: 'up' | 'down' }
+  | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
+  switch (action.type) {
+    case 'sort': {
+      const copy = [...state];
 
-            return state // need to fix
+      copy.sort((a, b) => {
+        if (action.payload === 'up') {
+          return a.name > b.name ? 1 : -1;
+        } else {
+          return a.name < b.name ? 1 : -1;
         }
-        case 'check': {
-
-            return state // need to fix
-        }
-        default:
-            return state
+      })
+      return copy;
     }
+
+    case 'check': {
+      return state
+        .filter(u => u.age >= action.payload)
+    }
+
+    default:
+      return state
+  }
 }
