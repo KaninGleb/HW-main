0515d1784a2b32332bd77841543b98d8af966bf7
Kanin Gleb <gleb.kanin@gmail.com>
2025-05-08 16:31:03 +0300

    HW10 - HW10 styles added

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import {useDispatch, useSelector} from 'react-redux'
+import {selectIsLoading} from './bll/selectors/loadingSelectors';
 import {loadingAC} from './bll/loadingReducer'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
-import s2 from '../../s1-main/App.module.css'
 import {Loader} from './Loader'
-import {selectIsLoading} from './bll/selectors/loadingSelectors';
+import s from './Loader.module.css'
+import s2 from '../../s1-main/App.module.css'
 
 
 // * 1 - в файле loadingReducer.ts дописать типы и логику
 // * 2 - получить isLoading из редакса
 // * 3 - дописать функцию setLoading
 // * 4 - сделать стили в соответствии с дизайном
+
+const LOADING_TIMING = 1500;
 
 const HW10 = () => {
   const isLoading = useSelector(selectIsLoading)
@@ -20,17 +23,17 @@ const HW10 = () => {
     dispatch(loadingAC(true))
     setTimeout(() => {
       dispatch(loadingAC(false))
-    }, 1500)
+    }, LOADING_TIMING)
   }
 
   return (
-    <div id={'hw10'}>
+    <div id={'hw10'} className={s.hw10}>
       <div className={s2.hwTitle}>Homework #10</div>
 
-      <div className={s2.hw}>
+      <div className={`${s.hw}`}>
         {isLoading ? (
           <div id={'hw10-loading'}>
-            <Loader/>
+            <Loader timing={LOADING_TIMING}/>
           </div>
         ) : (
           <SuperButton
