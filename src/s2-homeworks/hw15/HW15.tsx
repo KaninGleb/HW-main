5147f4b8e98188f432c855bc44f064fa1594feb2
Kanin Gleb <gleb.kanin@gmail.com>
2025-06-24 15:34:31 +0300

    HW15 - onChangeSort fixed

--- src/s2-homeworks/hw15/HW15.tsx
+++ src/s2-homeworks/hw15/HW15.tsx
@@ -78,15 +78,20 @@ const HW15 = () => {
   }
 
   const onChangeSort = (newSort: string) => {
-    // делает студент
+    setSort(newSort)
+    setPage(1)
 
-    // setSort(
-    // setPage(1) // при сортировке сбрасывать на 1 страницу
+    sendQuery({
+      sort: newSort,
+      page: 1,
+      count,
+    })
 
-    // sendQuery(
-    // setSearchParams(
-
-    //
+    setSearchParams({
+      sort: newSort,
+      page: '1',
+      count: String(count),
+    })
   }
 
   useEffect(() => {
